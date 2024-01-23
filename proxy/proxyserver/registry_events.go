c166cb24830b83b35035e9a5f7fb1eae19cf4148
shilazi <sunshuaids@live.com>
2024-01-23 15:14:30 +0800

    Fix preheat handle event with recursion
    * docker manifestlist and oci index processed recursively util media type be manifest

--- proxy/proxyserver/registry_events.go
+++ proxy/proxyserver/registry_events.go
@@ -2,6 +2,31 @@ package proxyserver
 
 import "time"
 
+//{
+//    "Events": [
+//        {
+//            "Id": "YSnFhJWCpNoVynYgHQuyz2xdoyXMI7BD",
+//            "TimeStamp": "2024-01-18T14:37:42.194082639Z",
+//            "Action": "push",
+//            "Target": {
+//                "MediaType": "application/vnd.docker.distribution.manifest.v2+json",
+//                "Digest": "sha256:3eacb2401d9859bb89af468200241bbd46645c3d63e85f16393759ca4aa1c18e",
+//                "Repository": "test/image",
+//                "Url": "https://foo.example.com/v2/test/image/manifests/v1.0.2",
+//                "Tag": "v1.0.2"
+//            },
+//            "Request": null,
+//            "Actor": null
+//        }
+//    ]
+//}
+
+// TODO: There is a bug about registry event.
+// 		Target.MediaType always 'application/vnd.docker.distribution.manifest.v2+json'.
+// 		Whatever push with any other media type, such as:
+// 		* application/vnd.oci.image.index.v1+json
+// 		* application/vnd.docker.distribution.manifest.list.v2+json
+
 // Notification holds all events. refer to https://docs.docker.com/registry/notifications/.
 type Notification struct {
 	Events []Event
